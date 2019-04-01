1006312b9e108967318e1c10da22825d5e9cd440
unknown <bramosjaneway@gmail.com>
2019-04-01 10:43:28 -0500

    creation of customer class and customer controller with get commands for a single customer and a list of customers

---
+++ BangazonAPI/Controllers/CustomerController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BangazonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public CustomerController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // GET: api/Customer
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using(SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, FirstName, LastName FROM Customer";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Customer> customers = new List<Customer>();
+                    while (reader.Read())
+                    {
+                        customers.Add(new Customer
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName"))
+                        });
+                    }
+
+                    reader.Close();
+                    return Ok(customers);
+                }
+            }
+        }
+
+        // GET: api/Customer/5
+        [HttpGet("{id}", Name = "GetCustomer")]
+        public IActionResult GetCustomer(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT FirstName, LastName 
+                                            FROM Customer
+                                            WHERE Id = @Id";
+                    cmd.Parameters.Add(new SqlParameter("@Id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Customer customer = null;
+                    if (reader.Read())
+                    {
+                        customer = new Customer
+                        {
+                            Id = id,
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName"))
+                        };
+                    }
+
+                    reader.Close();
+                    return Ok(customer);
+                }
+            }
+        }
+
+        // POST: api/Customer
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Customer/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
